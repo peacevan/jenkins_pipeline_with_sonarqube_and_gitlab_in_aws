2bc50647800f19ac8b30d1d93fda41dec8544215
IVAN AMADO CARDOSO <peacevan@hotmail.com>
2026-05-24 21:19:48 -0300

    chore(iac): avoid writing private key by default; remove hardcoded AMI/subnet defaults

--- modules/aws_key_pair/variables.tf
+++ modules/aws_key_pair/variables.tf
@@ -1,3 +1,9 @@
 variable "namespace" {
   type = string
 }
+
+variable "create_local_key_file" {
+  type        = bool
+  description = "Whether to write the generated private key to a local file. Default false to avoid accidental secret leakage."
+  default     = false
+}
--- modules/ec2_gitlab/variables.tf
+++ modules/ec2_gitlab/variables.tf
@@ -9,14 +9,14 @@ variable "instance_type" {
         default = "t2.micro"
 }
 
+
 variable "subnet_id" {
         description = "The VPC subnet the instance(s) will be created in"
-        default = "subnet-07ebbe60"
 }
 
 variable "ami_id" {
-        description = "The AMI to use"
-        default = "ami-09d56f8956ab235b3"
+        description = "The AMI to use (leave empty to use module default lookup)"
+        default     = ""
 }
 
 variable "number_of_instances" {
@@ -26,7 +26,7 @@ variable "number_of_instances" {
 
 
 variable "ami_key_pair_name" {
-        default = "my_terrafor init ec2"
+        default = ""
 }
 
 variable "key_name" {
